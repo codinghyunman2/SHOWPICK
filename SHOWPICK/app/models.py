2185c4a75df64b3a38e588060270c9a2aeb27382
lyujeehyun <jeehyun9808@naver.com>
2020-06-07 04:11:58 +0900

    jeehyun_04:11

--- SHOWPICK/app/models.py
+++ SHOWPICK/app/models.py
@@ -22,9 +22,6 @@ class Custom_user(models.Model):
     location_dong = models.TextField()
     email = models.TextField()
 
-    def __str__(self):
-        return self.name
-
 class Vote(models.Model):
     owner = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "vote")
     image = models.TextField(null = True)
@@ -33,18 +30,29 @@ class Vote(models.Model):
     location_dong = models.TextField(null = True)
     title = models.TextField(null = True)
 
-class ConventionVote(models.Model):
-    big_category = models.TextField(null = True)
-    small_category = models.TextField(null = True)
-    location_dong = models.TextField(null = True)
-    title = models.TextField(null = True)
+######영범 수정본############
+class ConventionBigVote(models.Model):
+    category = models.TextField
     vote_count = models.IntegerField()
+
+class ConventionSmallVote(models.Model):
+    category = models.TextField
+    vote_count = models.IntegerField()
+
+class ConventionTitleVote(models.Model):
+    category = models.TextField
+    vote_count = models.IntegerField()
+
+#####영범 수정본 ###############
+
 
 class Temporary_Big_Category(models.Model):
     category = models.TextField()
 
 class Temporary_Small_Category(models.Model):
     category = models.TextField()
+
+
 
 class Question(models.Model):
   name = models.CharField(max_length=200)
