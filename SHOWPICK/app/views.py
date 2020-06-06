dae3b41df9f99ce2d835fde6a393e2277e524462
codinghyunman2 <tkso1357@naver.com>
2020-06-07 03:43:01 +0900

    또 안됨

--- SHOWPICK/app/views.py
+++ SHOWPICK/app/views.py
@@ -9,7 +9,7 @@ import csv
 import os
 
 
-# Create your views here.
+# Create your views here. 
 
 def home(request):
 
@@ -20,7 +20,6 @@ def customer_map(request):
     return render(request, "Customer_map.html")
 
 def customer_map_Anam(request):
-
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     TEMP_DIR = os.path.join(BASE_DIR, "app", "data", "store.csv")
 
@@ -38,7 +37,7 @@ def customer_map_Anam(request):
         Temporary_Big_Category.objects.create(
             category = i1
         )
-        Show_Big_Category = Temporary_Big_Category.objects.all()
+    Show_Big_Category = Temporary_Big_Category.objects.all()
     
     if request.method == "POST":
         print(request.POST)
@@ -55,10 +54,9 @@ def customer_map_Anam(request):
     return render(request, 'customer_map_Anam.html', {'Show_Big_Category':Show_Big_Category})
 
 def customer_small_category(request,vote_pk):
-
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    TEMP_DIR = os.path.join(BASE_DIR, "app", "data", "store.csv")    
-    
+    TEMP_DIR = os.path.join(BASE_DIR, "app", "data", "store.csv")  
+
     semi_vote= Vote.objects.get(pk=vote_pk)
     check_big_category = semi_vote.big_category
 
@@ -69,20 +67,44 @@ def customer_small_category(request,vote_pk):
     semi_small_category = []
 
     for semi in range(1, len(csv_data)):
-        if csv_data[semi][0] == checkbig_category:
-            semi_big_category.append(csv_data[semi][0])
-    set_semi_big_category = set(semi_big_category)
+        if csv_data[semi][0] == check_big_category:
+            semi_small_category.append(csv_data[semi][1])
+    set_semi_small_category = set(semi_small_category)
 
-    for i1 in set_semi_big_category:
+    for i1 in set_semi_small_category:
         Temporary_Small_Category.objects.create(
-            category = i2
+            category = i1
         )
-        Show_Small_Category = Temporary_Big_Category.objects.all()
-    return render(request, "customer_small_category.html", {"Show_Small_Category":Show_Small_Category})
+    Show_Small_Category = Temporary_Small_Category.objects.all()
+    vote = Vote.objects.get(pk=vote_pk)
+    Found_map1 = vote.location_dong
+    Found_map2 = vote.big_category
+    if request.method == "POST":
 
-def customer_title(reuqest):
+        Vote.objects.filter(pk=vote_pk).update(
+            #onwer = vote.owner,
+            #image =0,
+            #big_category = vote.big_category,
+            small_category = request.POST["Small_Category"],
+            #location_dong = vote.location_dong,
+            #title = 0
+        )
+        return redirect("customer_title", vote_pk)
+    return render(request, "customer_small_category.html", {"Show_Small_Category":Show_Small_Category, "Found_map1":Found_map1, "Found_map2":Found_map2})
+
+def customer_title(request,vote_pk):
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    TEMP_DIR = os.path.join(BASE_DIR, "app", "data", "store.csv")
+    TEMP_DIR = os.path.join(BASE_DIR, "app", "data", "store.csv")  
+
+
+    semi_vote= Vote.objects.get(pk=vote_pk)
+    check_small_category = semi_vote.small_category
+
+    Temporary_Small_Category.objects.all().delete()
+    with open(TEMP_DIR, newline='', encoding = "euc-kr") as csvfile:
+        csv_data = list(csv.reader(csvfile))
+
+    semi_title_category = []
 
     for semi in range(1, len(csv_data)):
         if csv_data[semi][1] == check_small_category:
@@ -100,11 +122,10 @@ def customer_title(reuqest):
     if request.method == "POST":
         vote = Vote.objects.get(pk=vote_pk)
         Vote.objects.filter(pk=vote_pk).update(
-            title = request.POST["title_category"]
+            title = request.POST["Title_Category"]
         )
-        return redirect("Customer_map_Anam")
+        return redirect("")
     return render(request, "customer_title.html", {"Show_Title_Category":Show_Title_Category, "Found_map1":Found_map1, "Found_map2":Found_map2})
-
 
 def customer_map_Jongam(request):
 
@@ -128,10 +149,26 @@ def testing_map(request):
 
     return render(request, "testing_map.html")
 
-def mypage(request):
-    custom_user = Custom_user.objects.all()
-    return render(request, "mypage.html")
-    
+
+def mypage(request, user_pk):
+    custom_user = Custom_user.objects.get(pk=user_pk)
+
+    return render(request, "mypage.html", { "custom_user": custom_user})
+
+def mypage_edit(request):
+    if request.method == 'POST':
+        Custom_user.objects.create(
+            real_user = request.user,
+            gender = request.POST['gender'],
+            age = request.POST['age'],
+            location_gu = request.POST['location_gu'],
+            location_dong = request.POST['location_dong'],
+            email = request.POST['email']
+        )
+        return redirect('mypage')
+    else:
+        return render(request, 'mypage_edit.html')
+
 def vote_home(request):
     return render(request, "vote_home.html")
 
