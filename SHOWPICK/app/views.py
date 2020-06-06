9485193f7e189ed95279c708fe4bd9442abeece0
lyujeehyun <jeehyun9808@naver.com>
2020-06-07 01:17:17 +0900

    conflict_solved

--- SHOWPICK/app/views.py
+++ SHOWPICK/app/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render, redirect
-<<<<<<< HEAD
-from .models import Location, Store, Question, Choice, Custom_user
-=======
 from .models import Location, Store, Custom_user, Vote,ConventionVote, Temporary_Big_Category, Temporary_Small_Category
 from django.contrib.auth.models import User
 from django.contrib import auth
@@ -22,11 +19,6 @@ import csv
 #         category = i1
     # )
 
-from .models import Location, Store, Question, Choice
->>>>>>> 268da7c172a70b12d5bbe9e61b7f8ddb557c5c08
-from django.contrib.auth.models import User
-from django.contrib import auth
-from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseRedirect
 from django.urls import reverse 
 
@@ -56,7 +48,7 @@ def customer_map_Anam(request):
         Temporary_Big_Category.objects.create(
             category = i1
         )
-    Show_Big_Category = Temporary_Big_Category.objects.all()
+        Show_Big_Category = Temporary_Big_Category.objects.all()
     
     if request.method == "POST":
         print(request.POST)
@@ -83,61 +75,20 @@ def customer_small_category(request,vote_pk):
     semi_small_category = []
 
     for semi in range(1, len(csv_data)):
-        if csv_data[semi][0] == check_big_category:
-            semi_small_category.append(csv_data[semi][1])
-    set_semi_small_category = set(semi_small_category)
+        if csv_data[semi][0] == checkbig_category:
+            semi_big_category.append(csv_data[semi][0])
+    set_semi_big_category = set(semi_big_category)
 
-    for i1 in set_semi_small_category:
+    for i1 in set_semi_big_category:
         Temporary_Small_Category.objects.create(
-            category = i1
+            category = i2
         )
-    Show_Small_Category = Temporary_Small_Category.objects.all()
-    vote = Vote.objects.get(pk=vote_pk)
-    Found_map1 = vote.location_dong
-    Found_map2 = vote.big_category
-    if request.method == "POST":
+        Show_Small_Category = Temporary_Big_Category.objects.all()
+    return render(request, "customer_small_category.html", {"Show_Small_Category":Show_Small_Category})
 
-        Vote.objects.filter(pk=vote_pk).update(
-            #onwer = vote.owner,
-            #image =0,
-            #big_category = vote.big_category,
-            small_category = request.POST["Small_Category"],
-            #location_dong = vote.location_dong,
-            #title = 0
-        )
-        return redirect("customer_title", vote_pk)
-    return render(request, "customer_small_category.html", {"Show_Small_Category":Show_Small_Category, "Found_map1":Found_map1, "Found_map2":Found_map2})
+def customer_title(reuqest):
 
-def customer_title(request,vote_pk):
-    semi_vote= Vote.objects.get(pk=vote_pk)
-    check_small_category = semi_vote.small_category
-
-    Temporary_Small_Category.objects.all().delete()
-    with open('/mnt/c/Users/User/Programming/NEXT_LION/Idea-Hackerton/Hacekrton-1430/SHOWPICK/app/data/store.csv', newline='', encoding = "euc-kr") as csvfile:
-        csv_data = list(csv.reader(csvfile))
-
-    semi_title_category = []
-
-    for semi in range(1, len(csv_data)):
-        if csv_data[semi][1] == check_small_category:
-            semi_title_category.append(csv_data[semi][2])
-    set_semi_title_category = set(semi_title_category)
-
-    for i1 in set_semi_title_category:
-        Temporary_Small_Category.objects.create(
-            category = i1
-        )
-    Show_Title_Category = Temporary_Small_Category.objects.all()
-    vote = Vote.objects.get(pk=vote_pk)
-    Found_map1 = vote.location_dong
-    Found_map2 = vote.small_category
-    if request.method == "POST":
-        vote = Vote.objects.get(pk=vote_pk)
-        Vote.objects.filter(pk=vote_pk).update(
-            title = request.POST["Title_Category"]
-        )
-        return redirect("")
-    return render(request, "customer_title.html", {"Show_Title_Category":Show_Title_Category, "Found_map1":Found_map1, "Found_map2":Found_map2})
+    return render(request, "customer_title", {"Show_Small_Category":Show_Small_Category})
 
 
 
@@ -163,7 +114,6 @@ def testing_map(request):
 
     return render(request, "testing_map.html")
 
-<<<<<<< HEAD
 def mypage(request, user_pk):
     custom_user = Custom_user.objects.get(pk=user_pk)
 
@@ -178,23 +128,6 @@ def mypage_edit(request):
             location_gu = request.POST['location_gu'],
             location_dong = request.POST['location_dong'],
             email = request.POST['email'],
-=======
-
-def mypage(request):
-    if (request.method == 'POST'):
-        found_user = auth.authenticate(
-            username = request.POST['username'],
-            password = request.POST['password']
-        )
-        if (found_user is None):
-            error = '아이디 또는 비밀번호가 틀렸습니다'
-            return render(request, 'registration/login.html', {'error': error })
-
-        auth.login(
-            request, 
-            found_user,
-            backend='django.contrib.auth.backends.ModelBackend'
->>>>>>> 268da7c172a70b12d5bbe9e61b7f8ddb557c5c08
         )
         return redirect('mypage')
     else:
