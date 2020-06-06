652a0dbd72199a0e5ac59885bba6664a532f5c99
lyujeehyun <jeehyun9808@naver.com>
2020-06-07 03:07:52 +0900

    mypage_simplify

--- SHOWPICK/app/admin.py
+++ SHOWPICK/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Choice, Question, Vote
+from .models import Choice, Question, Vote, Custom_user
 # Register your models here.
 admin.site.register(Choice)
 admin.site.register(Question)
 admin.site.register(Vote)
+admin.site.register(Custom_user)
+
